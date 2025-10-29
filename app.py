a77ec0afa72f4057d4cad5fd9ef9f60ec2bb29c0
RoyXaler22 <salva220502@gmail.com>
2025-10-29 13:34:33 -0500

    4 Pr

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
-from flask import Flask, request, jsonify
-import pandas as pd
+from flask import Flask, jsonify
 
 app = Flask(__name__)
-df = pd.read_parquet("Funnel_Exportado.parquet")
 
-@app.route("/consulta", methods=["GET"])
-def consulta():
-    filtros = request.args
-    resultado = df.copy()
-    for col, val in filtros.items():
-        if col in resultado.columns:
-            resultado = resultado[resultado[col].astype(str) == val]
-    return jsonify(resultado.to_dict(orient="records"))
+@app.route('/')
+def home():
+    return jsonify({"message": "¡Hola desde tu API en Netlify!"})
+
+if __name__ == '__main__':
+    app.run()
